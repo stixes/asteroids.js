50d56b87cd6fccad7a65280166d5ef486d5e0f6c
Jesper Mathiassen <jesper.mathiassen@gmail.com>
2018-10-29 09:29:13 +0100

    Architekture improvements

--- AsteroidsJS.js
+++ AsteroidsJS.js
@@ -10,16 +10,6 @@
 // [x] Highscore list
 // [ ] Serverside highscore
 
-var ship;
-var world;
-var bg;
-var level;
-var score;
-var menu = true;
-var menuTime = 0;
-var highscore = [[10000,"zap"],[9000,"zip"],[8000,"pif"],[7000,"nag"],[6000,"snu"],[4000,"exo"],[2000,"hck"],[1000,"ino"],[500,"ikr"],[100,"wtf"]]
-var initials = "AAA";
-
 function setup() {
   createCanvas(windowWidth,windowHeight);
   noSmooth();
@@ -34,10 +24,8 @@ function setup() {
   world.add(new Roid());
   world.add(new Roid());
   world.add(new Roid());
-  initials = "";
-  initials += random("ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(''));
-  initials += random("ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(''));
-  initials += random("ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(''));
+  var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+  initials = random(letters) + random(letters) + random(letters);
 }
 
 function startMenu() {
---
+++ globals.js
@@ -0,0 +1,9 @@
+var ship;
+var world;
+var bg;
+var level;
+var score;
+var menu = true;
+var menuTime = 0;
+var highscore = [[10000,"zap"],[9000,"zip"],[8000,"pif"],[7000,"nag"],[6000,"snu"],[4000,"exo"],[2000,"hck"],[1000,"ino"],[500,"ikr"],[100,"wtf"]]
+var initials = "AAA";
